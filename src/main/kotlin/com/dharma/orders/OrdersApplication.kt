794a282c9f393a84604f3f1775de9b1ee3b56a64
rowanfoo <rowanfoo@gmail.com>
2021-09-23 00:50:28 +0800

    first commit

---
+++ src/main/kotlin/com/dharma/orders/OrdersApplication.kt
@@ -0,0 +1,49 @@
+package com.dharma.orders
+
+import com.dharma.orders.manager.EmailManager
+import com.dharma.orders.rules.RulesManager
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
+import java.time.LocalDateTime
+
+@SpringBootApplication
+@ComponentScan(basePackages = ["com.dharma.orders", "com.dhamma.pesistence"])
+@EnableScheduling
+
+class OrdersApplication : CommandLineRunner {
+
+    @Autowired
+    lateinit var emailManager: EmailManager
+
+
+    @Autowired
+    lateinit var rulesManager: RulesManager
+
+
+    override fun run(vararg args: String?) {
+     //     emailManager.run()
+//        rulesManager.run()
+    }
+
+    @Scheduled(cron = "0 15 15 ? * MON-FRI")
+    fun reportCurrentTime() {
+        println("---------RUN------${LocalDateTime.now()}-----")
+        emailManager.run()
+    }
+
+    @Scheduled(cron = "0 3 15 ? * MON-FRI")
+    fun runporttest() {
+        println("---------RUN------${LocalDateTime.now()}-----")
+        rulesManager.run()
+    }
+
+}
+
+fun main(args: Array<String>) {
+    runApplication<OrdersApplication>(*args)
+}
